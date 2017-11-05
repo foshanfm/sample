2a5d22dcabd7dfe31c4d441753695d8e7618836d
bombe <fungkinpong2013@gmail.com>
2017-11-05 03:47:36 +0000

    Finish register

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    //11.5
+    public function create()
+    {
+        return view('users.create');
+    }
+
 }
